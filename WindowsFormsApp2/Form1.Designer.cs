16842a48f1dc713de98c9f7d8cdfcabba45c8bb5
marco121942 <marco.sotomayor@tecsup.edu.pe>
2019-08-19 20:49:32 -0500

    first commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CboAnios = new System.Windows.Forms.ComboBox();
+            this.DgPedidos = new System.Windows.Forms.DataGridView();
+            this.neptunoDataSet = new WindowsFormsApp2.neptunoDataSet();
+            this.clientesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.clientesTableAdapter = new WindowsFormsApp2.neptunoDataSetTableAdapters.clientesTableAdapter();
+            this.LblNumero = new System.Windows.Forms.Label();
+            this.DgDetalle = new System.Windows.Forms.DataGridView();
+            this.LblMonto = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DgPedidos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.neptunoDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgDetalle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CboAnios
+            // 
+            this.CboAnios.FormattingEnabled = true;
+            this.CboAnios.Location = new System.Drawing.Point(365, 26);
+            this.CboAnios.Name = "CboAnios";
+            this.CboAnios.Size = new System.Drawing.Size(121, 21);
+            this.CboAnios.TabIndex = 0;
+            this.CboAnios.SelectionChangeCommitted += new System.EventHandler(this.CboAnios_SelectionChangeCommitted);
+            // 
+            // DgPedidos
+            // 
+            this.DgPedidos.AllowUserToAddRows = false;
+            this.DgPedidos.AllowUserToDeleteRows = false;
+            this.DgPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgPedidos.Location = new System.Drawing.Point(57, 90);
+            this.DgPedidos.Name = "DgPedidos";
+            this.DgPedidos.ReadOnly = true;
+            this.DgPedidos.Size = new System.Drawing.Size(541, 150);
+            this.DgPedidos.TabIndex = 1;
+            this.DgPedidos.DoubleClick += new System.EventHandler(this.DgPedidos_DoubleClick);
+            // 
+            // neptunoDataSet
+            // 
+            this.neptunoDataSet.DataSetName = "neptunoDataSet";
+            this.neptunoDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // clientesBindingSource
+            // 
+            this.clientesBindingSource.DataMember = "clientes";
+            this.clientesBindingSource.DataSource = this.neptunoDataSet;
+            // 
+            // clientesTableAdapter
+            // 
+            this.clientesTableAdapter.ClearBeforeFill = true;
+            // 
+            // LblNumero
+            // 
+            this.LblNumero.AutoSize = true;
+            this.LblNumero.Location = new System.Drawing.Point(563, 243);
+            this.LblNumero.Name = "LblNumero";
+            this.LblNumero.Size = new System.Drawing.Size(35, 13);
+            this.LblNumero.TabIndex = 2;
+            this.LblNumero.Text = "label1";
+            this.LblNumero.Click += new System.EventHandler(this.LblNumero_Click);
+            // 
+            // DgDetalle
+            // 
+            this.DgDetalle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgDetalle.Location = new System.Drawing.Point(57, 274);
+            this.DgDetalle.Name = "DgDetalle";
+            this.DgDetalle.Size = new System.Drawing.Size(541, 131);
+            this.DgDetalle.TabIndex = 3;
+            // 
+            // LblMonto
+            // 
+            this.LblMonto.AutoSize = true;
+            this.LblMonto.Location = new System.Drawing.Point(563, 424);
+            this.LblMonto.Name = "LblMonto";
+            this.LblMonto.Size = new System.Drawing.Size(35, 13);
+            this.LblMonto.TabIndex = 4;
+            this.LblMonto.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(214, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "seleccionar el año del pedido";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "LISTA DE PEDIDOS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "LISTA DE DETALLE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(513, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "TOTAL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(519, 423);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "TOTAL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 470);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LblMonto);
+            this.Controls.Add(this.DgDetalle);
+            this.Controls.Add(this.LblNumero);
+            this.Controls.Add(this.DgPedidos);
+            this.Controls.Add(this.CboAnios);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgPedidos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.neptunoDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgDetalle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CboAnios;
+        private System.Windows.Forms.DataGridView DgPedidos;
+        private neptunoDataSet neptunoDataSet;
+        private System.Windows.Forms.BindingSource clientesBindingSource;
+        private neptunoDataSetTableAdapters.clientesTableAdapter clientesTableAdapter;
+        private System.Windows.Forms.Label LblNumero;
+        private System.Windows.Forms.DataGridView DgDetalle;
+        private System.Windows.Forms.Label LblMonto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
